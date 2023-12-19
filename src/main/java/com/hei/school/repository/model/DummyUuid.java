a7eb14e1d89bcc7bea00068fc63be57986f631d9
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-12-19 11:56:21 +0300

    feat: initializer project

---
+++ src/main/java/com/hei/school/repository/model/DummyUuid.java
@@ -0,0 +1,15 @@
+package com.hei.school.repository.model;
+
+import com.hei.school.PojaGenerated;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@PojaGenerated
+@Entity
+@Getter
+@Setter
+public class DummyUuid {
+  @Id private String id;
+}
---
+++ src/test/java/com/hei/school/conf/SqliteConf.java
@@ -0,0 +1,17 @@
+package com.hei.school.conf;
+
+import com.hei.school.PojaGenerated;
+import org.springframework.test.context.DynamicPropertyRegistry;
+
+@PojaGenerated
+public class SqliteConf {
+
+  void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("driverClassName", () -> "org.sqlite.JDBC");
+    registry.add(
+        "spring.jpa.database-platform", () -> "com.hei.school.repository.conf.SqliteDialect");
+    registry.add("spring.datasource.url", () -> "jdbc:sqlite:/tmp/sqlite-data:db?cache=shared");
+    registry.add("spring.datasource.username", () -> "sa");
+    registry.add("spring.datasource.password", () -> "sa");
+  }
+}
